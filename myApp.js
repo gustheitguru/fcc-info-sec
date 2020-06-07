ec1e97f8b4cfcf9049eecc2abf50270224f49a4b
Glitch (gustheitguru-fcc-info-sec-1) <none>
2020-06-07 03:35:38 +0000

    📺🐜 Updated with Glitch

--- myApp.js
+++ myApp.js
@@ -17,6 +17,8 @@ var app = express();              // Do Not Edit
 // Express apps by setting various HTTP headers.
 // Install the package, then require it.
 
+var helmet = require('helmet');
+
 
 
 /** 2) Hide potentially dangerous information - `helmet.hidePoweredBy()` */
@@ -31,7 +33,7 @@ var app = express();              // Do Not Edit
 
 // Use `helmet.hidePoweredBy()``
 
-
+app.use(helmet.hidePoweredBy());
 
 /** 3) Mitigate the risk of clickjacking - `helmet.frameguard()` */
 
@@ -46,7 +48,7 @@ var app = express();              // Do Not Edit
 // We don't need our app to be framed, so you should use `helmet.frameguard()`
 // passing to it the configuration object `{action: 'deny'}`
 
- 
+ app.use(helmet.frameguard({action: 'deny'}));
 
 /** 4) Mitigate the risk of XSS - `helmet.xssFilter()` */
 
@@ -69,7 +71,7 @@ var app = express();              // Do Not Edit
 
 // Use `helmet.xssFilter()`
 
-
+app.use(helmet.xssFilter());
 
 /** 5) Avoid inferring the response MIME type - `helmet.noSniff()` */
 
@@ -82,7 +84,7 @@ var app = express();              // Do Not Edit
 
 // Use `helmet.noSniff()`
 
-
+app.use(helmet.noSniff());
 
 /** 6) Prevent IE from opening *untrusted* HTML - `helmet.ieNoOpen()` */
 
@@ -95,7 +97,7 @@ var app = express();              // Do Not Edit
 
 // Use `helmet.ieNoOpen()`
 
-
+app.use(helmet.ieNoOpen());
 
 /**  7) Ask browsers to access your site via HTTPS only - `helmet.hsts()` */
 
@@ -115,6 +117,7 @@ var app = express();              // Do Not Edit
 
 var ninetyDaysInSeconds = 90*24*60*60;
 
+app.use(helmet.hsts({maxAge: ninetyDaysInSeconds, force: true}));
 
 //**Note**:
 // Configuring HTTPS on a custom website requires the acquisition of a domain,
@@ -133,7 +136,7 @@ var ninetyDaysInSeconds = 90*24*60*60;
 
 // Use `helmet.dnsPrefetchControl()`
 
-
+app.use(helmet.dnsPrefetchControl());
 
 /** 9) Disable Client-Side Caching - `helmet.noCache()` */
 
@@ -144,7 +147,7 @@ var ninetyDaysInSeconds = 90*24*60*60;
 // use this option only when there is a real need.
 
 // Use helmet.noCache()
-
+app.use(helmet.noCache());
 
 
 /** 10) Content Security Policy - `helmet.contentSecurityPolicy()` */
